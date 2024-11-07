8bb736eb06d2032b2e6930986e80c6b412e8f036
owenck <owenck@byu.edu>
2024-11-07 15:57:35 -0700

    convert header and footer to react

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './tagster.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body bg-dark text-light'>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <ul className="navbar-menu">
+                    <li>
+                        <a href='index.html' className="navbar-item">Home</a>
+                    </li>
+                <li>
+                        <a href='friends.html' className="navbar-item">Friends</a>
+                </li>
+                    <li>
+                        <a href='about.html' className="navbar-item">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <main>Main content goes here</main>
+
+        <footer>
+            <div>Author: Owen Keeler</div>
+            <a href="https://github.com/owenck/startup" className="githublink">My GitHub Repository</a>
+        </footer>
+    </div>
+  );
 }
